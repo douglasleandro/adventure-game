1a1d8ec79c65bba4d71d76c70143dfa81ccb76fb
Douglas-Fernandes-Leandro <douglaslfernandes@gmail.com>
2020-09-22 12:21:16 +0100

    Defines scenario functions

--- adventure_game.py
+++ adventure_game.py
@@ -25,26 +25,67 @@ def valid_input(prompt, opt1, opt2):
         else:
             return prompt
 
-intro()
+def field():
+    print_pause("Enter 1 to knock on the door of the house.")
+    print_pause("Enter 2 to peer into the cave.")
+    print_pause("What would you like to do? ")
 
-print_pause("Enter 1 to knock on the door of the house."
-            "Enter 2 to peer into the cave."
-            "What would you like to do? ")
 
-while True:
+def house(item):
+    print_pause("You approach the door of the house.")
+    print_pause("You are about to knock when the door opens and out steps a "
+                "pirate/gorgon/troll/wicked fairie.")
+    print_pause("Eep! This is the pirate/gorgon/troll/wicked fairie´s house!")
+    print_pause("The pirate/gorgon/troll/wicked fairie attacks you!")
+    print_pause("You feel a bit under-prepared for this, what with only "
+                "having a tiny dagger.")
 
-    option = valid_input("(Please enter 1 or 2).\n", '1', '2')
+
+    option = valid_input("Would you like to (1) fight or (2) run away?",
+                         "1", "2")
 
     if option == '1':
-        print ("You approach the door of the house.")
-        break
+        print_pause("You do your best...")
+        print_pause("but your dagger is no match for the "
+                    "pirate/gorgon/trollwicked fairie.")
+        print_pause("You have been defeated!")
+        option = valid_input("Would you like to play again? (y/n)", "y", "n")
     elif option == '2':
-        print ("You peer cautiously into the cave. "
-               "It turns out to be only a very small cave. "
-               "Your eye catches a glint of metal behind a rock."
-               "You have found the magical Sword of Ogoroth!"
-               "You discard your silly old dagger and take the sword with you."
-               "You walk back out to the field.")
-        break
+        print_pause("You run back into the field. Luckily, you don't seem to "
+                    "have been followed.")
+
+def cave(item):
+    if "sword" in item:
+        print_pause("You peer cautiously into the cave.")
+        print_pause("You've been here before, and gotten all the good stuff. "
+                    "It's just an empty cave now.")
+        print_pause("You walk back out to the field.\n")
+        field()
     else:
+        print_pause("You peer cautiously into the cave.")
+        print_pause("It turns out to be only a very small cave.")
+        print_pause("Your eye catches a glint of metal behind a rock.")
+        print_pause("You have found the magical Sword of Ogoroth!")
+        print_pause("You discard your silly old dagger and take the sword with "
+                    "you.")
+        item.append("sword")
+        print_pause("You walk back out to the field.\n")
+        field()
+
+def play_game():
+    item = []
+    while True:
+
         option = valid_input("(Please enter 1 or 2).\n", '1', '2')
+
+        if option == '1':
+            house(item)
+            break
+        elif option == '2':
+            cave(item)
+            break
+        else:
+            option = valid_input("(Please enter 1 or 2).\n", '1', '2')
+
+intro()
+play_game()
